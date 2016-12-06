4cd623667cb2f162d873606093a02f50bbc767fd
Ian Mitchell <ian.mitchel1@live.com>
2016-12-06 09:35:06 -0800

    Day 6

---
+++ 06/part2.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('input.txt')
+});
+
+rl.on('line', parseLine);
+rl.on('close', outputAnswer);
+
+const mapList = []
+
+function parseLine(text) {
+  while (mapList.length < text.length) {
+    mapList.push(new Map());
+  }
+
+  Array.from(text).forEach((char, index) => {
+    if (mapList[index].has(char)) {
+      mapList[index].set(char, mapList[index].get(char) + 1);
+    } else {
+      mapList[index].set(char, 1);
+    }
+  });
+}
+
+function outputAnswer() {
+  let answer = '';
+  
+  for (let i = 0; i < mapList.length; i++) {
+    answer += Array.from(mapList[i]).sort((a, b) => a[1] - b[1])[0][0];
+  }
+
+  console.log(`The word is ${answer}.`);
+}
